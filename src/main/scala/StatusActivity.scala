638c117bd9424cffcb09ac3931cb11bcd4b84982
Yuji Yokoo <yujiyokoo@gmail.com>
2012-10-28 22:58:32 +1030

    Implement "Post to yamba" feature

--- src/main/scala/StatusActivity.scala
+++ src/main/scala/StatusActivity.scala
@@ -1,13 +1,34 @@
 package yamba.scala.project
 
+import _root_.winterwell.jtwitter.Twitter
+
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import _root_.android.util.Log
+import _root_.android.view.View
+import _root_.android.view.View.OnClickListener
+import _root_.android.widget.Button
+import _root_.android.widget.EditText
 
-class StatusActivity extends Activity with TypedActivity {
+class StatusActivity extends Activity with TypedActivity with OnClickListener {
+  private val TAG:String = "StatusActivity"
+  var twitter: Twitter = null
+  var editText: EditText = null
+  var updateButton: Button = null
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-    findView(TR.textview).setText("hello, world!")
+    editText = findViewById( R.id.editText ).asInstanceOf[ EditText ]
+    updateButton = findViewById( R.id.buttonUpdate ).asInstanceOf[ Button ]
+    updateButton.setOnClickListener( this )
+    twitter = new Twitter( "student", "password" )
+    twitter.setAPIRootUrl( "http://yamba.marakana.com/api" )
+  }
+
+  override def onClick( v: View ) {
+   twitter.setStatus( editText.getText.toString )
+   Log.d( TAG, "onClicked" )
   }
 }
